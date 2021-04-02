17c107daaf24fb17ddb0224ace9a1de736385ed8
Alexander Ross <alexanderross936@gmail.com>
2021-04-02 12:50:44 -0500

    cors

--- server.js
+++ server.js
@@ -22,10 +22,14 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 app.get('/user', auth, async(req, res) => {
     try {
