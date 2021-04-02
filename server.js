d92a1772088a7867e86a641d74f989d708f43a1f
Alexander Ross <alexanderross936@gmail.com>
2021-04-02 13:17:52 -0500

    cors

--- server.js
+++ server.js
@@ -324,8 +324,6 @@ app.post('/add_recipe', auth, async(req, res) => {
 
 })
 
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-
 app.listen(process.env.PORT || 4000, () => {
     console.log('App listening on PORT 4000')
 })
